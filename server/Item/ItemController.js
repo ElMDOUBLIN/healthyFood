0541364a1ba4dc79d8e8a6bd18525b9b07861f3f
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-10-06 10:05:07 +0100

    test backend with postman done all work

--- server/Item/ItemController.js
+++ server/Item/ItemController.js
@@ -4,50 +4,50 @@ var Item = require('./Item');
 // create new item
 exports.createOne = function (req, res) {
     Item.create(req.body).then((result) => {
-        res.state(200).send(result);
+        res.send(result);
     }).catch((err) => {
-        res.state(404).send(err)
+        res.send(err)
     })
 };
 // get all items
 exports.retrieve = function (req, res) {
     Item.find({}).then((result) => {
-        res.state(200).send(result)
+        res.send(result)
     }).catch((err) => {
-        res.state(404).send(err)
+        res.send(err)
     })
 };
 // get one item
 exports.retrieveOne = function (req, res) {
     Item.findOne({ number: req.params.number }).then((result) => {
-        res.state(200).send(result)
+        res.send(result)
     }).catch((err) => {
-        res.state(404).send(err)
+        res.send(err)
     })
 };
 // update one item
 exports.updateOne = function (req, res) {
     Item.findOneAndUpdate({ number: req.params.number }, req.body).then((result) => {
-        res.state(200).send(result)
+        res.json(result)
     }).catch((err) => {
-        res.state(404).send(err)
+        res.send(err)
     })
 
 };
 // delete one item
 exports.deleteOne = function (req, res) {
     Item.deleteOne({ number: req.params.number }).then((result) => {
-        res.state(200).send(result)
+        res.send(result)
     }).catch((err) => {
-        res.state(404).send(err)
+        res.send(err)
     })
 };
 
 // delete all items
 exports.deleteAll = function (req, res) {
-    Item.deleteAll({}).then((result) => {
-        res.state(200).send(result)
+    Item.deleteMany({}).then((result) => {
+        res.send(result)
     }).catch((err) => {
-        res.state(404).send(err)
+        res.send(err)
     })
 }
